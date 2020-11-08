08ac0692721492d80e5f53015daf7b6f3eb7fec6
Your Name <you@example.com>
2020-11-08 15:26:43 -0800

    added mark as done function

--- src/data.js
+++ src/data.js
@@ -4,27 +4,31 @@ export const data = [
         tag: "Speaking",
         content: "Talk about the happiest day in your life",
         response: "",
-        created_at: ""
+        created_at: "",
+        done: false
     },
     {
         id: 2,
         tag: "Quote",
         content: "Sitting quietly, doing nothing, Spring comes, and the grass grows by itself",
         response: "",
-        created_at: ""
+        created_at: "",
+        done: false
     },
     {
         id: 3,
         tag: "Writing",
         content: "Why do people care so much about what others think about them?",
         response: "",
-        created_at: ""
+        created_at: "",
+        done: false
     },
     {
         id: 4,
         tag: "Writing",
         content: "Should we always dress up before going out?",
         response: "",
-        created_at: ""
+        created_at: "",
+        done: false
     }
 ]
--- src/tasks/tasks.jsx
+++ src/tasks/tasks.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import './tasks.scss';
 import { data } from '../data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 export default class Tasks extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-
+            answer: "",
+            clickedID: 0
         }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        
+    }
+
+    componentDidMount(){
+        //get all undone tasks
+    }
+
+    handleChange(e){
+        this.setState({
+            answer: e.target.value,
+        })
+    }
+
+    handleSubmit(e, id){
+        e.preventDefault()
+        console.log(this.state.answer)
+        //post to the task and then call function of getting all tasks again
+        data[id -1].response = this.state.answer
+        console.log('data', data)
+    }
+
+    markAsDone(){
+        //patch to update done status
     }
 
     render(){
+        console.log('data', data)
+
         return (
             <div className="container">
                 <div className="wrapper">
@@ -20,18 +51,33 @@ export default class Tasks extends React.Component {
                                 {each.tag !== "Quote"
                                 ? <div className="tag">
                                     <p className={each.tag}>{each.tag}</p>
+                                    <FontAwesomeIcon
+                                        icon = {faCheckSquare}
+                                        className="done-icon"
+                                        onClick = {() => this.markAsDone()}
+                                    />
                                   </div>
                                 : null}
                                 <div className="task-description">
                                     <p id={each.tag}>{each.content}</p>
                                 </div>
                                 {each.tag === "Writing"
-                                ? <div className="response">
-                                    <textarea
-                                        type="text"
-                                        placeholder="Start typing..."
-                                    />
-                                  </div>
+                                ? each.response.length === 0
+                                    ? <div className="response">
+                                        <textarea
+                                            type="text"
+                                            placeholder="Start typing..."
+                                            onChange={this.handleChange}
+                                        />
+                                        <button 
+                                            onClick={e => this.handleSubmit(e, each.id)}
+                                            className="submit-btn"
+                                        >Submit</button>
+                                    </div>
+                                    : <div className="response-result">
+                                        <p className="text">{each.response}</p>
+                                        <button className="edit-btn">Edit</button>
+                                      </div>
                                 : null}
                             </div>
                         )}
